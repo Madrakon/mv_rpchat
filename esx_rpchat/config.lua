a2c727de3068ad74cd370f16bb5fd9be48640586
Matouš Větrovec <90977968+Madrakon@users.noreply.github.com>
2022-12-13 21:09:49 +0100

    Add files via upload

---
+++ esx_rpchat/config.lua
@@ -0,0 +1,24 @@
+Config        = {}
+Config.Locale = 'en'
+
+Config.OnlyICNames = true 
+Config.OnlyFirstname     = true
+Config.EnableTweet       = true
+
+
+
+
+Config.AnnounceMinutes = 30 -- Minutes 
+
+Config.Messages = {
+	{message = "you gonna find the text at the top at server/main 45 and 62 "},
+}
+
+Config.AnnounceMinutes2 = 30  -- Minutes 
+
+Config.Messages2 = {
+	{message = "you gonna find this text in config.lua"},
+}
+
+
+
---
+++ esx_rpchat/locales/en.lua
@@ -0,0 +1,16 @@
+Locales['en'] = {
+  
+  ['ooc_prefix'] = '^0[^3OOC - LOCAL^0] ^5@%s^0',
+  ['global_ooc_prefix'] = '[OOC] %s',
+  ['twt_help'] = 'Send message to Twitter.',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = '',
+  ['me_prefix'] = '[ME] %s',
+  ['do_help'] = '',
+  ['do_prefix'] = '[DO] %s',
+  ['bm_prefix'] = 'Are you sure you wanna write to BM?.',
+  ['generic_argument_name' ] = 'message',
+  ['generic_argument_help'] = 'message',
+  ['unknown_command'] = '^3%s^0 isn't valid argument!',
+  ['doc_cooldown'] = 'wait for a second'
+}
